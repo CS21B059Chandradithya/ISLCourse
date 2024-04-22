bde350200f18447a30e3b58ae9fcfcf59d979a89
CS21B059Chandradithya <110807516+CS21B059Chandradithya@users.noreply.github.com>
2024-04-22 15:51:30 +0530

    Create hub_conf.py

---
+++ hub_conf.py
@@ -0,0 +1,98 @@
+import torch
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor, ToPILImage
+from PIL import Image
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+from sklearn.metrics import classification_report, confusion_matrix, precision_score
+
+def load_data():
+
+    # Download training data from open datasets.
+    training_data = datasets.FashionMNIST(
+        root="data",
+        train=True,
+        download=True,
+        transform=ToTensor(),
+    )
+
+    # Download test data from open datasets.
+    test_data = datasets.FashionMNIST(
+        root="data",
+        train=False,
+        download=True,
+        transform=ToTensor(),
+    )
+    return training_data, test_data
+
+def create_dataloaders(training_data, test_data, batch_size=64):
+
+    # Create data loaders.
+    train_dataloader = DataLoader(training_data, batch_size=batch_size)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+    for X, y in test_dataloader:
+        print(f"Shape of X [N, C, H, W]: {X.shape}")
+        print(f"Shape of y: {y.shape} {y.dtype}")
+        break
+
+    return train_dataloader, test_dataloader
+
+class cs21b059(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.m = nn.Softmax(dim =1)
+        self.fc1 = nn.Linear(28*28*1, 120)
+        self.fc2 = nn.Linear(120, 10)
+
+    def forward(self, x):
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        x = self.m(x)
+        return x
+
+#train the network
+def train_network(train_loader, optimizer,criteria, e):
+  for epoch in range(e):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = model(inputs)
+        #print(outputs.shape, labels.shape)
+        tmp = torch.nn.functional.one_hot(labels, num_classes= 10)
+        loss = criteria(outputs, tmp)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+            running_loss = 0.0
+
+  print('Finished Training')
+
+#cross entropy
+def loss_fun(y_pred, y_ground):
+  v = -(y_ground * torch.log(y_pred + 0.0001))
+  v = torch.sum(v)
+  return v
+
+#write the get model
+def get_model_advanced(train_loader, n_epochs=10, lr=1e-4):
+	model = cs21b059()
+	optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+	criteria = loss_fun
+	train_network(train_loader, optimizer, criteria, n_epochs)
+	return model
